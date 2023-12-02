97f142509c9cd8e2f185461f180b3a2f439bcfe6
Ifath Rahman Tushar <110590857+irtushar@users.noreply.github.com>
2023-12-02 13:01:36 +0600

    Week-1 : Module-1

---
+++ O_N_squre.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) // O(N*N)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout << "Hello" << endl;
+        }
+    }
+
+    // same
+    // for(int i=0; i<=n-1; i++) // O(N*N)
+    // {
+    //     for(int j=i+1; j<n; j++)
+    //     {
+    //         cout << "Hello" << endl;
+    //     }
+    // }
+    return 0;
+}
